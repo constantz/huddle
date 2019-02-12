1196535dbf9536ef421f928c318517f54239d92d
Constant <c.zwijnenberg@gmail.com>
2019-02-12 16:54:13 +0100

    Seeders aangemaakt nog zonder inhoud

---
+++ database/seeds/Sent_MessagesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Sent_MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
