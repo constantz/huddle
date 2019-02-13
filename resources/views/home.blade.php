5e5fad53d02241211b91744bd61b60c913dcb36e
vagrant <vagrant@homestead>
2019-02-13 15:02:08 +0000

    if you log on you can go to the student or teacher page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,26 @@
 @section('content')
 <div class="container">
         @if(\Session::has('error'))
-        <div class=”alert alert-danger”>
+        <div class="alert alert-danger">
         {{\Session::get('error')}}
         </div>
         @endif
         <div class=”row”>
-        <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-            <div class=”panel-heading”>Dashboard</div>
-            <a href="#">Admin</a>
-        
-        
-        </div>
-        </div>
-        </div>
-       
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                        <?php if(auth()->user()->isAdmin == 1): ?>
+                            <div class=”panel-body”>
+                                <a href="{{url(‘admin/routes’)}}">Teacher</a>
+                            </div>
+                        <?php else: ?>
+                        <div class=”panel-body”>
+                            <a href="{{url('/progress')}}">Student</a>
+                        </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div> 
+        </div>    
 </div>
 @endsection
