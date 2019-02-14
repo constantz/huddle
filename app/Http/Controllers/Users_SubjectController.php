1f2e10d872ee9ff71352652ec0b770916d29ff7e
Constant <c.zwijnenberg@gmail.com>
2019-02-14 16:44:17 +0100

    users_subject dingetje

---
+++ app/Http/Controllers/Users_SubjectController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Users_subject;
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersSubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+<<<<<<< HEAD:app/Http/Controllers/Users_SubjectController.php
+        //
+=======
+        $usersSubjects = Users_subject::all();
+        
+        return view('/users/students', compact('usersSubjects'));
+        
+>>>>>>> 330a018f143da1327ce100c8ab00ef720e2a6ad0:app/Http/Controllers/UsersSubjectController.php
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $us = new Users_subject();
+        $us->user_id = Auth::user()->id;
+        $us->subject_id = Auth::subject()->id;
+        $us->passed = 1;
+
+        $us->save();
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Users_subject  $usersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Users_subject $usersubject)
+    {
+        $us = Users_subject::where('id', "*")->get();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Users_subject  $usersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Users_subject $usersubject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Users_subject  $usersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Users_subject $usersubject)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Users_subject  $usersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Users_subject $usersubject)
+    {
+        //
+    }
+}
--- app/Users_subject.php
+++ app/Users_subject.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Users_subject extends Model
 {
+<<<<<<< HEAD
+    protected $table = "users_subjects";
+	protected $fillable = ['user_id', 'subject_id', 'passed'];
+=======
     public function subject()
        {
         return $this->belongsTo(Subject::class);
        } 
+>>>>>>> 330a018f143da1327ce100c8ab00ef720e2a6ad0
 }
