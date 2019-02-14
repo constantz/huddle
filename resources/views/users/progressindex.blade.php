b98576fbedb22be79590095dda07ff7a2f395f42
Simone Wildenborg <s.wildenborg@gmail.com>
2019-02-14 09:23:26 +0100

    added checkboxes

--- resources/views/users/progressindex.blade.php
+++ resources/views/users/progressindex.blade.php
@@ -1,15 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="title">Students</h1>
+    <h1 class="title">Students</h1>
 
-    <ul>
+    <table>
+            <tr>
+                <th>Naam</th>
+                <th>HTML</th>
+                <th>CSS</th>
+                <th>JavaScript</th>
+                <th>PHP</th>
+            </tr>
         @foreach($students as $student)
-            <li>
-                <a href="/progress/{{ $student->id }}">
-                    {{ $student->name }}
-                </a>
-            </li>
+            <tr>
+                <td>
+                    <a href="/progress/{{ $student->id }}">{{ $student->name }}</a>
+                </td>
+            <form action="">
+                <td>
+                    <input type="checkbox" name="html">
+                </td>
+                <td>
+                    <input type="checkbox" name="css">
+                </td>
+                <td>
+                    <input type="checkbox" name="javascript">
+                </td>
+                <td>
+                    <input type="checkbox" name="php">
+                </td>
+            </form>
+            </tr>
         @endforeach
-    </ul>
-    @endsection
+    </table>
+@endsection
