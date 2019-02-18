720409191c7aba256bc97885c86471b39ab7dc34
Simone Wildenborg <s.wildenborg@gmail.com>
2019-02-18 14:00:53 +0100

    edited database

---
+++ app/Http/Controllers/EdumatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EdumatController extends Controller
+{
+    public function (edumat){
+
+    return view('edumat')
+    }
+}
---
+++ resources/views/users/edumat.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+
+@section('content')
+
+<h2>Hallo</h2>
+
+@endsection
+
+
+
+
