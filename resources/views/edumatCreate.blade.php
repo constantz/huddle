bab9b942a9e04aee2508cfa1307391feb56f2242
Constant <c.zwijnenberg@gmail.com>
2019-03-11 10:01:11 +0100

    create edumats improved

--- resources/views/edumatCreate.blade.php
+++ resources/views/edumatCreate.blade.php
@@ -5,9 +5,13 @@
 
     <form method="POST" action="/edumat">
         @csrf
-        <input type="integer" name = "subject_id" placeholder="vakid">
-        <input type="text" name = "name" placeholder="naam">
-        <input Type="longtext"  name = "content" placeholder = "inhoud">
+        <select name="subject_id">
+            @foreach($subjects as $subj)
+                <option value={{ $subj->id }}>{{ $subj->name }}</option>
+            @endforeach
+        </select>
+        <input type="text" name = "name" placeholder="naam" required>
+        <input Type="longtext"  name = "content" placeholder = "inhoud" required>
         <button type="submit">save</button>
     </form>
      
